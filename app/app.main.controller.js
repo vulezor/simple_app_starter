70f2aa6f8370b3eb346c3a6e36b8a46d555f7f8a
vulezor <vulezor@gmail.com>
2017-11-06 09:54:31 +0100

    initial commit

---
+++ app/app.main.controller.js
@@ -0,0 +1,7 @@
+(function(){
+    var mainController = function($scope){
+        
+    };
+    mainController.$inject = ['$scope']
+    angular.module("app").controller('mainController', mainController);
+})();
